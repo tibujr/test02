e1f2023e116936a82d5e44b89464459c1376fabd
tibujr <maycol_to@hotmail.com>
2016-05-31 16:40:30 -0500

    creando

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeoLocation.js",
+        "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeoLocation",
+        "pluginId": "cordova-plugin-mauron85-background-geolocation",
+        "clobbers": [
+            "backgroundGeoLocation"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-mauron85-background-geolocation": "0.9.6"
+}
+// BOTTOM OF METADATA
+});
